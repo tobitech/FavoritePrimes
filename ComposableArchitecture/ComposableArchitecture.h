a2f22bb38239ba638a84a6ec12c1fb5394e3f8e0
Oluwatobi Omotayo <tobitech@ymail.com>
2022-05-31 15:28:40 +0100

    Add new modules for favorite primes and composable architecture.

---
+++ ComposableArchitecture/ComposableArchitecture.h
@@ -0,0 +1,18 @@
+//
+//  ComposableArchitecture.h
+//  ComposableArchitecture
+//
+//  Created by Oluwatobi Omotayo on 31/05/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ComposableArchitecture.
+FOUNDATION_EXPORT double ComposableArchitectureVersionNumber;
+
+//! Project version string for ComposableArchitecture.
+FOUNDATION_EXPORT const unsigned char ComposableArchitectureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ComposableArchitecture/PublicHeader.h>
+
+
