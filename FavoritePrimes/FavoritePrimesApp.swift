db26860f16d206427f76467438c435d992b46cf5
Oluwatobi Omotayo <tobitech@ymail.com>
2022-05-31 12:44:40 +0100

    Initial Commit

---
+++ FavoritePrimes/FavoritePrimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritePrimesApp.swift
+//  FavoritePrimes
+//
+//  Created by Oluwatobi Omotayo on 31/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct FavoritePrimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
