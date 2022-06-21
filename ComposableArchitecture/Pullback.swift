73ebfd58b68d14cb0cce62200a865b7e11b67b87
Oluwatobi Omotayo <tobitech@ymail.com>
2022-06-21 09:06:37 +0100

    Introduce the concept of environment into the reducers themselves because they are responsible for producing side effects. This makes things more controllable.

--- ComposableArchitecture/Pullback.swift
+++ ComposableArchitecture/Pullback.swift
@@ -9,14 +9,15 @@ import Foundation
 
 /// One of two functions that form the foundation of reducer composition.
 /// This lets us transform a reducer that understands local state and actions into one that understands global states and actions.
-public func pullback<LocalValue, GlobalValue, LocalAction, GlobalAction>(
-  _ reducer: @escaping Reducer<LocalValue, LocalAction>,
+public func pullback<LocalValue, GlobalValue, LocalAction, GlobalAction, LocalEnvironment, GlobalEnvironment>(
+  _ reducer: @escaping Reducer<LocalValue, LocalAction, LocalEnvironment>,
   value: WritableKeyPath<GlobalValue, LocalValue>,
-  action: WritableKeyPath<GlobalAction, LocalAction?>
-) -> Reducer<GlobalValue, GlobalAction> {
-  return { globalValue, globalAction in
+  action: WritableKeyPath<GlobalAction, LocalAction?>,
+  environment: @escaping (GlobalEnvironment) -> LocalEnvironment
+) -> Reducer<GlobalValue, GlobalAction, GlobalEnvironment> {
+  return { globalValue, globalAction, globalEnvironment in
     guard let localAction = globalAction[keyPath: action] else { return [] }
-    let localEffects = reducer(&globalValue[keyPath: value], localAction)
+    let localEffects = reducer(&globalValue[keyPath: value], localAction, environment(globalEnvironment))
     
     return localEffects.map { localEffect in
       localEffect.map { localAction -> GlobalAction in
@@ -24,13 +25,6 @@ public func pullback<LocalValue, GlobalValue, LocalAction, GlobalAction>(
         globalAction[keyPath: action] = localAction
         return globalAction
       }.eraseToEffect()
-//      Effect { callback in
-//        localEffect.run { localAction in
-//          var globalAction = globalAction
-//          globalAction[keyPath: action] = localAction
-//          callback(globalAction)
-//        }
-//      }
     }
   }
 }
